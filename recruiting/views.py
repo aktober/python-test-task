c973c6c14af94af84c87bf673472267c8ff14d34
Anna Popovychenko <a.popovychenko@gmail.com>
2018-03-19 08:42:23 +0200

    debug

--- recruiting/views.py
+++ recruiting/views.py
@@ -5,6 +5,8 @@ from django.core.mail import send_mail
 def endpoint(request):
     if request.method == 'POST':
         aws_type = request.META.get('x-amz-sns-message-type')
+        print('aws_type', aws_type)
+        print(request.POST)
         if aws_type == 'SubscriptionConfirmation' or aws_type == 'Notification':
             subscription_url = request.POST.get('SubscribeURL')
             send_mail('Subscription URL', subscription_url, 'a.popovychenko@gmail.com',
